ed02d015a948743275fe94e1a7d01bada97f7bf7
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2023-07-13 01:55:06 +0300

    Create an API endpoint that selects a random greeting

--- app/controllers/greetings_controller.rb
+++
@@ -1,6 +0,0 @@
-class GreetingsController < ApplicationController
-  def random_greeting
-    message = Message.order('RANDOM()').first
-    render json: { greeting: message.greeting }
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'greetings/random_greeting'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    get '/random_greeting', to: 'greetings#random_greeting'
+  end
 end
